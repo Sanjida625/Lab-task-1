7bb64fb7cac7b36f0222c33dd97f1dc64196d255
Sanjida625 <sanjida.islam01819@gmail.com>
2024-09-11 15:04:04 +0600

    Add files via upload

---
+++ online.py
@@ -0,0 +1,110 @@
+print("Hello World!")
+
+print('\n')
+
+name = "Ritu"
+print(name) 
+
+print('\n')
+
+
+# An integer assignment
+age = 45
+
+# A floating point
+salary = 1456.8
+
+# A string
+name = "John"
+
+print(age)
+print(salary)
+print(name)
+
+print('\n')
+
+x = "Hello World" # string
+x = 50  # integer
+x = 60.5  # float
+x = 3j  # complex
+x = ["geeks", "for", "geeks"]  # list 
+x = ("geeks", "for", "geeks")  # tuple
+x = {"name": "Suraj", "age": 24} # dict
+x = {"geeks", "for", "geeks"} # set
+x = True  # bool
+x = b"Geeks" # binary
+
+print('\n')
+
+# Python program show input and Output
+val = input("Enter your value: ") 
+print(val) 
+
+print('\n')
+
+a = 9
+b = 4
+add = a + b 
+
+sub = a - b 
+
+mul = a * b 
+
+mod = a % b 
+
+p = a ** b 
+print(add) 
+print(sub) 
+print(mul) 
+print(mod) 
+print(p) 
+
+print('\n')
+
+
+a = True
+b = False
+print(a and b) 
+print(a or b) 
+print(not a) 
+
+print('\n')
+
+
+a = 10
+b = 4
+print(a & b) 
+print(a | b) 
+print(~a) 
+print(a ^ b) 
+print(a >> 2) 
+print(a << 2) 
+
+print('\n')
+
+
+a = 10
+b = a 
+print(b) 
+b += a 
+print(b) 
+b -= a 
+print(b) 
+b *= a 
+print(b) 
+b <<= a 
+print(b)
+
+print('\n')
+
+
+i = 20
+if (i < 15): 
+    print("i is smaller than 15") 
+    print("i'm in if Block") 
+else: 
+    print("i is greater than 15") 
+    print("i'm in else Block") 
+print("i'm not in if and not in else Block") 
+
+print('\n')
